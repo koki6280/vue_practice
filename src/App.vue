4e55cc8a796402a018ff00d9f66803adb6217545
林幸輝 <K-h.too.brag.is.idiot@docomo.ne.jp>
2020-10-21 00:27:27 +0900

    props

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
  <div>
   <like-header></like-header>
-  <LikeNumber></LikeNumber>
+  <h2>{{ number }}</h2>
+  <LikeNumber :total-number="number"></LikeNumber>
+  <LikeNumber :total-number="number"></LikeNumber>
  </div>
 </template>
 
 <script>
 import LikeHeader from "./components/LikeHeader.vue";
 export default {
+  data() {
+    return {
+      number: 14
+    };
+  },
   components: {
     LikeHeader
   }
